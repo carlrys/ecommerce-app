e0084ed0f1524952efe318ee56d57c4d6032d5dd
Carl David Reyes <cdreyes.bposeats@gmail.com>
2025-01-15 20:02:52 +0800

    added new button component for carousel

---
+++ src/components/EmblaButtons.tsx
@@ -0,0 +1,92 @@
+import React, {
+    ComponentPropsWithRef,
+    useCallback,
+    useEffect,
+    useState
+} from 'react'
+import { EmblaCarouselType } from 'embla-carousel'
+
+type UsePrevNextButtonsType = {
+    prevBtnDisabled: boolean
+    nextBtnDisabled: boolean
+    onPrevButtonClick: () => void
+    onNextButtonClick: () => void
+}
+
+export const usePrevNextButtons = (
+    emblaApi: EmblaCarouselType | undefined
+): UsePrevNextButtonsType => {
+    const [prevBtnDisabled, setPrevBtnDisabled] = useState(true)
+    const [nextBtnDisabled, setNextBtnDisabled] = useState(true)
+
+    const onPrevButtonClick = useCallback(() => {
+        if (!emblaApi) return
+        emblaApi.scrollPrev()
+    }, [emblaApi])
+
+    const onNextButtonClick = useCallback(() => {
+        if (!emblaApi) return
+        emblaApi.scrollNext()
+    }, [emblaApi])
+
+    const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+        setPrevBtnDisabled(!emblaApi.canScrollPrev())
+        setNextBtnDisabled(!emblaApi.canScrollNext())
+    }, [])
+
+    useEffect(() => {
+        if (!emblaApi) return
+
+        onSelect(emblaApi)
+        emblaApi.on('reInit', onSelect).on('select', onSelect)
+    }, [emblaApi, onSelect])
+
+    return {
+        prevBtnDisabled,
+        nextBtnDisabled,
+        onPrevButtonClick,
+        onNextButtonClick
+    }
+}
+
+type PropType = ComponentPropsWithRef<'button'>
+
+export const PrevButton: React.FC<PropType> = (props) => {
+    const { children, ...restProps } = props
+
+    return (
+    <button
+        className="embla__button embla__button--prev"
+        type="button"
+        {...restProps}
+    >
+        <svg className="embla__button__svg" viewBox="137.718 -1.001 366.563 644">
+        <path
+            fill="currentColor"
+            d="M428.36 12.5c16.67-16.67 43.76-16.67 60.42 0 16.67 16.67 16.67 43.76 0 60.42L241.7 320c148.25 148.24 230.61 230.6 247.08 247.08 16.67 16.66 16.67 43.75 0 60.42-16.67 16.66-43.76 16.67-60.42 0-27.72-27.71-249.45-249.37-277.16-277.08a42.308 42.308 0 0 1-12.48-30.34c0-11.1 4.1-22.05 12.48-30.42C206.63 234.23 400.64 40.21 428.36 12.5z"
+        />
+        </svg>
+        {children}
+    </button>
+    )
+}
+
+export const NextButton: React.FC<PropType> = (props) => {
+    const { children, ...restProps } = props
+
+    return (
+    <button
+        className="embla__button embla__button--next"
+        type="button"
+        {...restProps}
+    >
+        <svg className="embla__button__svg" viewBox="0 0 532 532">
+        <path
+            fill="currentColor"
+            d="M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.796 36.208-13.796 50.002 0 22.928 22.947 206.395 206.507 229.332 229.454a35.065 35.065 0 0 1 10.326 25.126c0 9.2-3.393 18.26-10.326 25.2-45.865 45.901-206.404 206.564-229.332 229.52Z"
+        />
+        </svg>
+        {children}
+    </button>
+    )
+}
