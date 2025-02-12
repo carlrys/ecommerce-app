a4b62930daa1848528afbda9b1450d1ead60a431
Carl David Reyes <cdreyes.bposeats@gmail.com>
2025-02-12 17:07:40 +0800

    added new route for new page

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
-import ProductList  from './pages/ProductList';
 import Cart from './pages/Cart';
 import Login from './pages/Login';
+import ProductsPage from './pages/ProductsPage';
 
 const App: React.FC = () => {
   return(
@@ -13,7 +13,7 @@ const App: React.FC = () => {
             <Routes>
               <Route path="/login" element={<Login />}/>
               <Route path="/" element={<Home />} />
-              <Route path="/products" element={<ProductList />}/>
+              <Route path="/products" element={<ProductsPage />}/>
               <Route path="/card" element={<Cart />}/>
             </Routes>
           </div>
